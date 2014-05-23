d5d671e6f2c4fe986cf0c17b0bddc22bdb8f4334
Tom <tom.flitterman@hotmail.fr>
2014-05-23 20:19:17 +0200

    Menu

--- menu.py
+++ menu.py
@@ -1,8 +1,14 @@
-print('1. Traduction latin -> morse')
-print('2. Traduction morse -> latin')
-print('3. Quitter (n\importe quelle touche')
-
-ch = input('Votre choix : ')
+print('1 pour la traduction latin -> morse')
+print('2 pour la traduction morse -> latin')
+print('Autre pour quitter (n\'importe quelle touche)')
+ch = input('\nVotre choix : ')
+    
 
 if (ch == '1') :
     from latin_morse import *
+
+elif (ch == '2') :
+    from morse_latin import *
+
+else :
+    print('Au revoir !')
